a376275a1ab857cb3bc083a3bd0d5a74062f9c70
karthikkm007 <jacksparrow941994@gmail.com>
2019-01-12 11:28:20 +0530

    Revert "zram: close race by open overriding"
    This reverts commit ba28cad91a647e8a7be73afe3adb1fe4e0ba59f3.
    
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -115,9 +115,5 @@ struct zram {
 	 */
 	u64 disksize;	/* bytes */
 	char compressor[10];
-	/*
-	 * zram is claimed so open request will be failed
-	 */
-	bool claim; /* Protected by bdev->bd_mutex */
 };
 #endif
